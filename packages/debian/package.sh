2e783db72a45612a0c9f9e549c7dbbc90ab77ace
Yuki <yifan@liii.pro>
2026-02-06 13:34:13 +0800

    linux修改1

--- packages/debian/package.sh
+++ packages/debian/package.sh
@@ -11,7 +11,7 @@ ICON_SOURCE_REL="3rdparty/qwindowkitty/src/styles/app/stem.png"
 
 # 尝试获取 VERSION
 if [ -z "$VERSION" ]; then
-    VERSION="2025.2.3"
+    VERSION="2026.1.4"
 else
     echo "✅ 检测到版本号: $VERSION"
 fi
