3b000489498c29cfc98b38cf77915545022784f3
Yuki <yifan@liii.pro>
2026-02-12 11:26:23 +0800

    windows调试ok

--- src/Plugins/Qt/qt_sys_utils.cpp
+++ src/Plugins/Qt/qt_sys_utils.cpp
@@ -197,7 +197,25 @@ get_linux_or_macos_device_id () {
 #ifdef Q_OS_WINDOWS
 QString
 get_windows_device_id () {
-  return QStringLiteral ("000000000000");
+  DWORD serialNumber = 0;
+  BOOL success = GetVolumeInformationW (
+    L"C:\\",
+    NULL,
+    0,
+    &serialNumber,
+    NULL,
+    NULL,
+    NULL,
+    0
+  );
+
+  if (success && serialNumber != 0) {
+    QByteArray data = QByteArray::number (serialNumber, 16).toUpper ();
+    QByteArray hashed = QCryptographicHash::hash (data, QCryptographicHash::Sha256);
+    return QString (hashed.toHex ());
+  }
+
+  return "";
 }
 #endif
 
