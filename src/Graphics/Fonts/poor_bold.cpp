412363f828b64fe1a8c361b3ca3dae5de87ec93e
Yuki <yifan@liii.pro>
2026-03-27 10:17:24 +0800

    wip

--- src/Graphics/Fonts/poor_bold.cpp
+++ src/Graphics/Fonts/poor_bold.cpp
@@ -350,8 +350,8 @@ static double
 get_bold_factor (string font_name) {
   string lname= locase_all (font_name);
   if (occurs ("simsun", lname)) return 0.5;
-  if (occurs ("kaiti_gb2312", lname)) return 0.2;
   if (occurs ("simhei", lname)) return 0.5;
+  if (occurs ("kaiti_gb2312", lname)) return 0.2;
   if (occurs ("fangsong_gb2312", lname)) return 0.2;
   return 1.0;
 }
