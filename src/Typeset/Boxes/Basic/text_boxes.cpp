7a94876ea6d737c8b9e07a1873135bf79154647c
JackChen <17683835261@163.com>
2026-04-28 20:31:59 +0800

    半角符号压缩修复format

--- src/Typeset/Boxes/Basic/text_boxes.cpp
+++ src/Typeset/Boxes/Basic/text_boxes.cpp
@@ -127,9 +127,9 @@ text_box_rep::adjust_kerning (int mode, double factor) {
 
 box
 text_box_rep::right_contract_kerning (double factor) {
-  SI       pad= (SI) tm_round (factor * fn->wfn);
-  SI       blank= max ((SI) 0, x2 - x4);
-  pad          = min (pad, blank);
+  SI pad  = (SI) tm_round (factor * fn->wfn);
+  SI blank= max ((SI) 0, x2 - x4);
+  pad     = min (pad, blank);
   if (pad <= 0) return this;
   xkerning nxk (0, 0, 0);
   if (!is_nil_or_zero (xk)) {
@@ -145,9 +145,9 @@ text_box_rep::right_contract_kerning (double factor) {
 
 box
 text_box_rep::left_contract_kerning (double factor) {
-  SI       pad= (SI) tm_round (factor * fn->wfn);
-  SI       blank= max ((SI) 0, x3 - x1);
-  pad          = min (pad, blank);
+  SI pad  = (SI) tm_round (factor * fn->wfn);
+  SI blank= max ((SI) 0, x3 - x1);
+  pad     = min (pad, blank);
   if (pad <= 0) return this;
   xkerning nxk (0, 0, 0);
   if (!is_nil_or_zero (xk)) {
