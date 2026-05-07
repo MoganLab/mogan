504c0d6bb5b4248a0482cbc5d827260334a9d659
Da Shen <da@liii.pro>
2026-05-07 17:15:27 +0800

    wip

--- src/Plugins/Pdf/pdf_hummus_renderer.cpp
+++ src/Plugins/Pdf/pdf_hummus_renderer.cpp
@@ -1335,8 +1335,8 @@ void
 pdf_hummus_renderer_rep::draw (int glyph_index, font_glyphs fn, SI x, SI y,
                                  int codepoint) {
   // debug_convert << "draw \"" << (char)glyph_index << "\" " << glyph_index
-  //               << " " << fn->res_name << "\n";
-  if (codepoint == 0x200B) return;
+  //                << " " << fn->res_name << "\n";
+  if (glyph_index == 0x17 && starts (fn->res_name, "ecss10")) return;
   // emoji cache for this renderer instance
   static hashmap<index_type, picture> emoji_cache;
   if (is_emoji_character (glyph_index)) {
