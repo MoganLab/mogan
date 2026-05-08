f18b6092c5cb61e3257d1161b8c21ef2cc79dbc2
Da Shen <da@liii.pro>
2026-05-08 16:06:22 +0800

    wip

--- TeXmacs/progs/texmacs/texmacs/tm-view.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-view.scm
@@ -145,19 +145,17 @@
   (:check-mark "v" focus-mode?)
   (toggle-visible-header))
 
-(define saved-simplest-state '(#t #t #t))
+(define saved-simplest-state '(#t #t))
 
 (tm-define (toggle-simplest-mode)
   (:synopsis "Toggle simplest mode.")
   (:check-mark "v" simplest-mode?)
   (if (simplest-mode?)
       (begin
-        (show-header (car saved-simplest-state))
-        (show-icon-bar 1 (cadr saved-simplest-state))
-        (show-icon-bar 2 (caddr saved-simplest-state)))
+        (show-icon-bar 1 (car saved-simplest-state))
+        (show-icon-bar 2 (cadr saved-simplest-state)))
       (begin
-        (set! saved-simplest-state (list (visible-header?) (visible-icon-bar? 1) (visible-icon-bar? 2)))
-        (show-header #f)
+        (set! saved-simplest-state (list (visible-icon-bar? 1) (visible-icon-bar? 2)))
         (show-icon-bar 1 #f)
         (show-icon-bar 2 #f))))
 
