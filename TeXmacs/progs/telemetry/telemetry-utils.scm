b1cb5d0b9db175c7addf65e368c72716e90b3177
Yuki <yifan@liii.pro>
2026-05-11 20:29:22 +0800

    纠正埋点相关devel错误部分，修改buffer-size初始值

--- TeXmacs/progs/telemetry/telemetry-utils.scm
+++ TeXmacs/progs/telemetry/telemetry-utils.scm
@@ -24,7 +24,7 @@
 
 (import (only (srfi srfi-19) current-date date->string date-zone-offset))
 
-(define telemetry-buffer-size 30)
+(define telemetry-buffer-size 300)
 (define telemetry-flush-interval-ms 300000)
 (define-public telemetry-max-queue-size 1000)
 (define telemetry-meta-max-entries 200)
