2462fd1d4251a944c5a872174f7ec720b676e57b
AXeonV <2607343351@qq.com>
2026-05-11 21:45:14 +0800

    fix preview-ref numbering of figure/table

--- TeXmacs/progs/link/ref-edit.scm
+++ TeXmacs/progs/link/ref-edit.scm
@@ -398,13 +398,24 @@
          `(,(tm-label t) ,@(map clean-preview (tm-children t))))
         (else t)))
 
+(define (fix-fig_or_tb-number doc id)
+; preview 不是在原文档上下文里排版，而是把目标片段单独拿出来排版
+; 在排版 figure / table 时，这个临时区域的 figure-nr / table-nr 从 0 开始计数，因此预览框里的编号永远是 1
+; 这里用一个宏包装，the-figure / the-table 时当前 figure / table 计数器，用于生成显示用编号
+; 这里先通过 get-binding 从文档中获取真实编号，这样在 *-figure / *-table 展开渲染时就能获取正确的编号
+  (cond ((tree-in? doc '(small-figure big-figure))
+         `(with "the-figure" (macro (get-binding ,id)) ,doc))
+        ((tree-in? doc '(small-table big-table))
+         `(with "the-table" (macro (get-binding ,id)) ,doc))
+        (else doc)))
+
 (define (preview-expand-context? t)
   (tree-in? t '(theorem proposition lemma corollary conjecture
                 theorem* proposition* lemma* corollary* conjecture*
                 definition axiom
                 definition* axiom*)))
 
-(define (label-preview t)
+(define (label-preview t id)
   (and-with doc (tree-search-upwards t preview-context?)
     (with math? (tree-search-upwards t math-context?)
       (when (and (tree-up doc) (tree-up (tree-up doc))
@@ -418,6 +429,7 @@
       (when (tm-is? doc 'row)
         (set! doc (apply tmconcat (map uncell (tm-children doc)))))
       (set! doc (clean-preview doc))
+      (set! doc (fix-fig_or_tb-number doc id))
       (when math?
         (set! doc `(with "math-display" "true" (math ,doc))))
       `(preview-balloon ,doc))))
@@ -426,10 +438,10 @@
   (and-with p (and-nnull? (label->path id))
     (with t (path->tree (cDr p))
       (cond ((label-context? t)
-             (label-preview t))
+             (label-preview t id))
             ((tree-in? t '(glossary glossary-explain glossary-dup
                            index subindex subsubindex index-complex))
-             (label-preview t))
+             (label-preview t id))
             (else #f)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
