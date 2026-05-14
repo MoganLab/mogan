1b067f01e3671615c769bd50106a3cd4647f2a9c
Yuki <2622126521@qq.com>
2026-05-14 14:28:05 +0800

    Revert "[0607] 修复 APPENDIX 下公式编号前缀显示为 0 的问题 (#3350)"
    This reverts commit 0e89727fcda51d6cf84aed21257d5133973a2e52.

--- TeXmacs/packages/customize/theorem/number-long-article.ts
+++ TeXmacs/packages/customize/theorem/number-long-article.ts
@@ -27,7 +27,7 @@
 
   <drd-props|equation-prefix-sep|macro-parameter|regular>
 
-  <assign|display-std-env|<macro|nr|<section-prefix><arg|nr>>>
+  <assign|display-std-env|<macro|nr|<number|<value|section-nr>|arabic><equation-prefix-sep><arg|nr>>>
 
   \;
 </body>
