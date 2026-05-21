595dbbd12916ecd37aaaaabc159e18c8cb9be38e
MoonL79 <151734889+MoonL79@users.noreply.github.com>
2026-05-21 17:03:02 +0800

    format

--- src/Plugins/Tex/fromtex_post.cpp
+++ src/Plugins/Tex/fromtex_post.cpp
@@ -448,7 +448,8 @@ finalize_pmatrix (tree t) {
         else if (u[i][0] == "aligned")
           parse_pmatrix (r, u, i, "", "", "tabular*");
         else if (u[i][0] == "stack") parse_pmatrix (r, u, i, "", "", "stack");
-        else if (u[i][0] == "subarray") parse_pmatrix (r, u, i, "", "", "stack");
+        else if (u[i][0] == "subarray")
+          parse_pmatrix (r, u, i, "", "", "stack");
         else if (u[i][0] == "matrix")
           parse_pmatrix (r, u, i, "", "", "tabular*");
         else if (u[i][0] == "pmatrix")
