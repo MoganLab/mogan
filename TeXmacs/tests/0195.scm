8069e9c117f51b571c97e585524fe2777e7499da
Da Shen <da@liii.pro>
2026-06-08 23:15:50 +0800

    wip

--- TeXmacs/tests/0195.scm
+++ TeXmacs/tests/0195.scm
@@ -17,30 +17,41 @@
 (tm-define (test_0195)
   ;; (concat (document "text")) should simplify to just "text"
   (let* ((input '(concat (document "hello world")))
-         (result (tree->stree (tree-simplify (stree->tree input)))))
-    (check result => "hello world"))
+         (result (tree->stree (tree-simplify (stree->tree input))))
+        ) ;
+    (check result => "hello world")
+  ) ;let*
 
   ;; (concat (item) (document "text")) should simplify to (concat (item) "text")
   (let* ((input '(concat (item) (document "hello world")))
-         (result (tree->stree (tree-simplify (stree->tree input)))))
-    (check result => '(concat (item) "hello world")))
+         (result (tree->stree (tree-simplify (stree->tree input))))
+        ) ;
+    (check result => '(concat (item) "hello world"))
+  ) ;let*
 
   ;; (document (concat (item) (document "text"))) should simplify nested document
   (let* ((input '(document (concat (item) (document "hello world"))))
-         (result (tree->stree (tree-simplify (stree->tree input)))))
-    (check result => '(document (concat (item) "hello world"))))
+         (result (tree->stree (tree-simplify (stree->tree input))))
+        ) ;
+    (check result => '(document (concat (item) "hello world")))
+  ) ;let*
 
   ;; (concat (document "a") (document "b")) - two single-child documents
   ;; Each document is unwrapped, then adjacent text is merged: "ab"
   (let* ((input '(concat (document "a") (document "b")))
-         (result (tree->stree (tree-simplify (stree->tree input)))))
-    (check result => "ab"))
+         (result (tree->stree (tree-simplify (stree->tree input))))
+        ) ;
+    (check result => "ab")
+  ) ;let*
 
   ;; (concat (document "a" "b")) - multi-child document should NOT be unwrapped
   ;; simplify_concat on the document returns (document "a" "b"), then
   ;; N(r)==1 so it becomes just the single child (document "a" "b")
   (let* ((input '(concat (document "a" "b")))
-         (result (tree->stree (tree-simplify (stree->tree input)))))
-    (check result => '(document "a" "b")))
+         (result (tree->stree (tree-simplify (stree->tree input))))
+        ) ;
+    (check result => '(document "a" "b"))
+  ) ;let*
 
-  (check-report))
+  (check-report)
+) ;tm-define
