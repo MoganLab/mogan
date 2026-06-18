6c05a0be5b7d0a829a5c1c70fe5fbb7a0d55addf
MoonL79 <151734889+MoonL79@users.noreply.github.com>
2026-06-18 16:30:00 +0800

    [0367] 将 education 目录纳入格式化范围并执行格式化

--- TeXmacs/progs/education/edu-markup.scm
+++ TeXmacs/progs/education/edu-markup.scm
@@ -12,8 +12,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (texmacs-module (education edu-markup)
-  (:use (database title-markup)
-        (education edu-edit)))
+  (:use (database title-markup) (education edu-edit))
+) ;texmacs-module
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Customization of titles
@@ -21,82 +21,99 @@
 
 (define (title-prefix pre tit)
   (cond ((tm-is? tit 'doc-make-rich-title)
-	 (with l (tm-children tit)
-	   `(doc-make-rich-title ,@(cDr l) ,(title-prefix pre (cAr l)))))
-	((tm-is? tit 'document)
-	 `(document ,pre ,@(tm-children tit)))
-	(else tit)))
+         (with l
+           (tm-children tit)
+           `(doc-make-rich-title ,@(cDr l) ,(title-prefix pre (cAr l)))
+         ) ;with
+        ) ;
+        ((tm-is? tit 'document) `(document ,pre ,@(tm-children tit)))
+        (else tit)
+  ) ;cond
+) ;define
 
 (tm-define (doc-data-exam t xopts)
   (:secure #t)
-  (let* ((doc   (doc-data t xopts))
-	 (class (select t '(doc-exam-class :%1)))
-	 (date  (select t '(doc-exam-date :%1))))
-    (cond ((and (null? class) (null? date))
-	   doc)
-	  ((and (nnull? class) (null? date))
-	   (title-prefix `(doc-exam-class ,(car class)) doc))
-	  ((and (null? class) (nnull? date))
-	   (title-prefix `(doc-exam-date ,(car date)) doc))
-	  ((and (nnull? class) (nnull? date))
-	   (title-prefix
-	    `(doc-exam-class-date ,(car class) ,(car date)) doc)))))
+  (let* ((doc (doc-data t xopts))
+         (class (select t '(doc-exam-class :%1)))
+         (date (select t '(doc-exam-date :%1)))
+        ) ;
+    (cond ((and (null? class) (null? date)) doc)
+          ((and (nnull? class) (null? date))
+           (title-prefix `(doc-exam-class ,(car class)) doc)
+          ) ;
+          ((and (null? class) (nnull? date))
+           (title-prefix `(doc-exam-date ,(car date)) doc)
+          ) ;
+          ((and (nnull? class) (nnull? date))
+           (title-prefix `(doc-exam-class-date ,(car class) ,(car date)) doc)
+          ) ;
+    ) ;cond
+  ) ;let*
+) ;tm-define
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Table like
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (ext-tile-item t)
-  `(cell (document ,t)))
+  `(cell (document ,t))
+) ;define
 
 (define (ext-tiled-row l cols)
-  (let* ((r (map (lambda (x) "") (.. (length l) cols)))
-         (a (append l r)))
-    `(row ,@(map ext-tile-item a))))
-  
+  (let* ((r (map (lambda (x) "") (.. (length l) cols))) (a (append l r)))
+    `(row ,@(map ext-tile-item a))
+  ) ;let*
+) ;define
+
 (define (ext-tiled-rows l cols)
   (if (> (length l) cols)
-      (cons (ext-tiled-row (sublist l 0 cols) cols)
-            (ext-tiled-rows (sublist l cols (length l)) cols))
-      (list (ext-tiled-row l cols))))
+    (cons (ext-tiled-row (sublist l 0 cols) cols)
+      (ext-tiled-rows (sublist l cols (length l)) cols)
+    ) ;cons
+    (list (ext-tiled-row l cols))
+  ) ;if
+) ;define
 
 (tm-define (ext-tiled-items t c)
   (:secure #t)
   (let* ((cols (or (string->number (tree->string c)) 5))
-         (w (string-append (number->string (/ 0.999999 cols)) "par")))
-    `(tformat
-      (twith "table-valign" "T")
-      (twith "table-hmode" "min")
-      (twith "table-width" "1par")
-      (cwith "1" "-1" "1" "-1" "cell-hmode" "exact")
-      (cwith "1" "-1" "1" "-1" "cell-width" ,w)
-      (cwith "1" "-1" "1" "-1" "cell-hyphen" "t")
-      (cwith "1" "-1" "1" "-1" "cell-lsep" "-0.2ln")
-      (cwith "1" "-1" "1" "-1" "cell-rsep" "-0.2ln")
-      (cwith "1" "-1" "1" "-1" "cell-bsep" "0ln")
-      (cwith "1" "-1" "1" "-1" "cell-tsep" "0ln")
-      (table ,@(ext-tiled-rows (tree-children t) cols)))))
+         (w (string-append (number->string (/ 0.999999 cols)) "par"))
+        ) ;
+    `(tformat (twith "table-valign" "T")
+       (twith "table-hmode" "min")
+       (twith "table-width" "1par")
+       (cwith "1" "-1" "1" "-1" "cell-hmode" "exact")
+       (cwith ,"1" ,"-1" ,"1" ,"-1" ,"cell-width" ,w)
+       (cwith "1" "-1" "1" "-1" "cell-hyphen" "t")
+       (cwith "1" "-1" "1" "-1" "cell-lsep" "-0.2ln")
+       (cwith "1" "-1" "1" "-1" "cell-rsep" "-0.2ln")
+       (cwith "1" "-1" "1" "-1" "cell-bsep" "0ln")
+       (cwith "1" "-1" "1" "-1" "cell-tsep" "0ln")
+       (table ,@(ext-tiled-rows (tree-children t) cols)))
+  ) ;let*
+) ;tm-define
 
 (define (ext-vertical-item t)
-  `(row (cell (document ,t))))
+  `(row (cell (document ,t)))
+) ;define
 
 (tm-define (ext-vertical-items t out inn)
   (:secure #t)
-  `(tformat
-    (twith "table-valign" "T")
-    (twith "table-hmode" "min")
-    (twith "table-width" "1par")
-    (twith "table-lborder" ,(tree-copy out))
-    (twith "table-rborder" ,(tree-copy out))
-    (twith "table-tborder" ,(tree-copy out))
-    (twith "table-bborder" ,(tree-copy out))
-    (cwith "1" "-1" "1" "-1" "cell-hyphen" "t")
-    (cwith "1" "-1" "1" "-1" "cell-lsep" "0ln")
-    (cwith "1" "-1" "1" "-1" "cell-rsep" "0ln")
-    (cwith "1" "-1" "1" "-1" "cell-bsep" "-0.2ln")
-    (cwith "1" "-1" "1" "-1" "cell-tsep" "-0.2ln")
-    (cwith "2" "-1" "1" "-1" "cell-tborder" ,inn)
-    (table ,@(map ext-vertical-item (tree-children t)))))
+  `(tformat (twith "table-valign" "T")
+     (twith "table-hmode" "min")
+     (twith "table-width" "1par")
+     (twith ,"table-lborder" ,(tree-copy out))
+     (twith ,"table-rborder" ,(tree-copy out))
+     (twith ,"table-tborder" ,(tree-copy out))
+     (twith ,"table-bborder" ,(tree-copy out))
+     (cwith "1" "-1" "1" "-1" "cell-hyphen" "t")
+     (cwith "1" "-1" "1" "-1" "cell-lsep" "0ln")
+     (cwith "1" "-1" "1" "-1" "cell-rsep" "0ln")
+     (cwith "1" "-1" "1" "-1" "cell-bsep" "-0.2ln")
+     (cwith "1" "-1" "1" "-1" "cell-tsep" "-0.2ln")
+     (cwith ,"2" ,"-1" ,"1" ,"-1" ,"cell-tborder" ,inn)
+     (table ,@(map ext-vertical-item (tree-children t))))
+) ;tm-define
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Popups
@@ -104,27 +121,33 @@
 
 (define (mc-popup-first l)
   (and (nnull? l)
-       (with (h . t) l
-         (if (tm-func? h 'mc-field 2)
-             (tm-ref h 1)
-             (mc-popup-first t)))))
+    (with (h . t) l (if (tm-func? h 'mc-field 2) (tm-ref h 1) (mc-popup-first t)))
+  ) ;and
+) ;define
 
 (define (mc-popup-selected l)
   (and (nnull? l)
-       (with (h . t) l
-         (if (mc-field-active? h)
-             (tm-ref h 1)
-             (mc-popup-selected t)))))
+    (with (h . t) l (if (mc-field-active? h) (tm-ref h 1) (mc-popup-selected t)))
+  ) ;and
+) ;define
 
 (tm-define (ext-mc-popup t)
   (:secure #t)
-  (let* ((ch    (tm-children t))
+  (let* ((ch (tm-children t))
          (first (mc-popup-first ch))
-         (sel   (mc-popup-selected ch))
-         (text  (or (and sel `(mc-selected-field ,sel))
-                    (and first `(mc-selected-none ,first))
-                    `(mc-selected-none "---")))
-         (text* (or sel first "---")))
-    `(button-popup
-      ,text ,t (with "button-nr" "0" (vertical-items* ,@ch))
-      "left" "Bottom" "default")))
+         (sel (mc-popup-selected ch))
+         (text (or (and sel `(mc-selected-field ,sel))
+                 (and first `(mc-selected-none ,first))
+                 '(mc-selected-none "---")
+               ) ;or
+         ) ;text
+         (text* (or sel first "---"))
+        ) ;
+    `(button-popup ,text
+       ,t
+       (with ,"button-nr" ,"0" (vertical-items* ,@ch))
+       ,"left"
+       ,"Bottom"
+       ,"default")
+  ) ;let*
+) ;tm-define
