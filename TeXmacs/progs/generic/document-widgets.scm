2c7b01ada82492b01378fb9cf1a13fcd2773151c
AXeonV <2607343351@qq.com>
2026-07-03 14:56:17 +0800

    modify text prompt

--- TeXmacs/progs/generic/document-widgets.scm
+++ TeXmacs/progs/generic/document-widgets.scm
@@ -299,13 +299,19 @@
                            ) ;enum
                          ) ;item
                 ) ;aligned
-                (when (== range "Custom range")
-                  (aligned (item (text "Applying from:")
-                             (input (set! rfrom answer) "string" (list rfrom) "6em")
-                           ) ;item
-                    (item (text "Applying to:") (input (set! rto answer) "string" (list rto) "6em"))
-                  ) ;aligned
-                ) ;when
+        (when (== range "Custom range")
+          (aligned (item (text "Range:")
+                     (hlist (input (set! rfrom answer) "string" (list rfrom) "3em")
+                            //
+                            //
+                            (text "~")
+                            //
+                            //
+                            (input (set! rto answer) "string" (list rto) "3em")
+                     ) ;hlist
+                   ) ;item
+          ) ;aligned
+        ) ;when
                 (aligned (item (text "Number style:")
                            (enum (set! nt
                                    (cond ((== answer "1, 2, 3") "arabic")
