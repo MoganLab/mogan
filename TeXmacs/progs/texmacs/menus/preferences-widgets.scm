8186b65f7bb0773abd96db4cc5515ebd90eddc23
MoonL79 <151734889+MoonL79@users.noreply.github.com>
2026-07-07 11:23:03 +0800

    [0500] macOS 下魔法粘贴快捷键显示 Cmd 前缀

--- TeXmacs/progs/texmacs/menus/preferences-widgets.scm
+++ TeXmacs/progs/texmacs/menus/preferences-widgets.scm
@@ -103,6 +103,14 @@
  ("ctrl+v" "Ctrl+V")
 ) ;define-preference-names
 
+;; macOS 上 Ctrl 键实际由 Command 承担，展示串改用 Cmd 前缀。
+;; preference 内部存储保持 "ctrl+..." 不变（与 generic-kbd.scm 比对一致），
+;; 仅覆盖 encode 表影响首选项面板的显示。
+(when (os-macos?)
+  (set-preference-name "magic-paste-shortcut" "ctrl+shift+v" "Cmd+Shift+V")
+  (set-preference-name "magic-paste-shortcut" "ctrl+v" "Cmd+V")
+) ;when
+
 (tm-widget (general-preferences-widget)
   (aligned (item (text "Look and feel:")
              (enum (set-pretty-preference* "look and feel" answer)
@@ -165,7 +173,7 @@
     ) ;item
     (item (text "Magic paste shortcut:")
       (enum (set-pretty-preference* "magic-paste-shortcut" answer)
-        '("Ctrl+Shift+V" "Ctrl+V")
+        (if (os-macos?) '("Cmd+Shift+V" "Cmd+V") '("Ctrl+Shift+V" "Ctrl+V"))
         (get-pretty-preference "magic-paste-shortcut")
         "18em"
       ) ;enum
