dc6c4f86b406419f9ff6072bbec1356415f20570
AXeonV <2607343351@qq.com>
2026-07-15 18:14:53 +0800

    wip

--- TeXmacs/progs/graphics/graphics-menu.scm
+++ TeXmacs/progs/graphics/graphics-menu.scm
@@ -535,7 +535,13 @@
 ) ;menu-bind
 
 (menu-bind graphics-control-point-pos-menu
- ("Set position" (interactive graphics-set-control-point-position))
+  ("Set position"
+   (interactive
+     (lambda (x y) (graphics-set-control-point-position x y))
+     (list "X coordinate" "string" (get-control-point-x (stree-radical (car (sketch-get1))) selected-point-no))
+     (list "Y coordinate" "string" (get-control-point-y (stree-radical (car (sketch-get1))) selected-point-no))
+   ) ;interactive
+  ) ;"Set position"
 ) ;menu-bind
 
 (menu-bind graphics-line-width-menu
