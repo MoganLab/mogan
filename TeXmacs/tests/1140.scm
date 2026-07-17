fc307a0e5cbb7ff7fd44b0bbf33291621c101453
Da Shen <da@liii.pro>
2026-07-17 17:17:39 +0800

    Revert "[1140] 修正 make-graphics-over 光标路径，避免 graphics-zoom 收到 #f"
    This reverts commit 0f8cae7b8235346d0eaa51f210659845039bc427.

--- TeXmacs/tests/1140.scm
+++ TeXmacs/tests/1140.scm
@@ -19,8 +19,9 @@
 
 (define (raises-error? thunk)
   "Return #t if THUNK raises an error, #f otherwise."
-  (catch #t (lambda () (thunk) #f) (lambda args #t))
-) ;define
+  (catch #t
+    (lambda () (thunk) #f)
+    (lambda args #t)))
 
 (define (test-tm-upwards-path-invalid)
   (display "Testing tm-upwards-path with invalid path...\n")
@@ -28,28 +29,24 @@
   ;; 修复后：应直接返回 #f，不调用 path-exists?
   (check (raises-error? (lambda () (tm-upwards-path #f '(with) '()))) => #f)
   (check (tm-upwards-path #f '(with) '()) => #f)
-  (display "tm-upwards-path invalid path tests passed!\n")
-) ;define
+  (display "tm-upwards-path invalid path tests passed!\n"))
 
 (define (test-get-upwards-tree-property-invalid)
   (display "Testing get-upwards-tree-property with invalid path...\n")
   (check (raises-error? (lambda () (get-upwards-tree-property #f "x"))) => #f)
   (check (eq? (get-upwards-tree-property #f "x") nothing) => #t)
-  (display "get-upwards-tree-property invalid path tests passed!\n")
-) ;define
+  (display "get-upwards-tree-property invalid path tests passed!\n"))
 
 (define (test-graphics-eval-magnify-no-graphics)
   (display "Testing graphics-eval-magnify without graphics...\n")
   ;; headless 下没有 graphics 上下文，graphics-graphics-path 返回 #f
   (check (raises-error? (lambda () (graphics-eval-magnify))) => #f)
   (check (graphics-eval-magnify) => "default")
-  (display "graphics-eval-magnify without graphics tests passed!\n")
-) ;define
+  (display "graphics-eval-magnify without graphics tests passed!\n"))
 
 (tm-define (test_1140)
   (display "Running test_1140...\n")
   (test-tm-upwards-path-invalid)
   (test-get-upwards-tree-property-invalid)
   (test-graphics-eval-magnify-no-graphics)
-  (check-report)
-) ;tm-define
+  (check-report))
