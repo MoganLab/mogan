5f82cc3fc5fca8451a8ade97174652eb52b117ef
AXeonV <2607343351@qq.com>
2026-08-04 11:07:18 +0800

    wip

--- TeXmacs/progs/generic/diff-text.scm
+++ TeXmacs/progs/generic/diff-text.scm
@@ -62,9 +62,7 @@
            (stree-str (object->string origin-stree))
           ) ;
       (debug-message "debug-io"
-        (string-append "diff-request: stree=["
-          (substring stree-str 0 (min 500 (string-length stree-str)))
-          "]\n"
+        (string-append "diff-request: stree=[" (herk->utf8 stree-str) "]\n"
         ) ;string-append
       ) ;debug-message
       (diff-cloud-polish stree-str
@@ -82,9 +80,7 @@
           (selection-active?)
         ) ;and
     (debug-message "debug-io"
-      (string-append "diff-predict: suggested=["
-        (substring suggested-str 0 (min 500 (string-length suggested-str)))
-        "]\n"
+      (string-append "diff-predict: suggested=[" (herk->utf8 suggested-str) "]\n"
       ) ;string-append
     ) ;debug-message
     (let ((suggested-stree (catch #t (lambda () (string->object suggested-str)) (lambda args #f))
