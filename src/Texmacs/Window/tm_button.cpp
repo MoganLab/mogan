c7b8dbad965faa06d5cf17979218ea14970aa2a2
pigmagicfly <2831850183@qq.com>
2026-08-04 10:58:08 +0800

    [2081] 修复数学模式下字母 T 的 Tab 循环显示

--- src/Texmacs/Window/tm_button.cpp
+++ src/Texmacs/Window/tm_button.cpp
@@ -253,9 +253,12 @@ box_widget (scheme_tree p, string s, color col, bool trans, bool ink) {
   if ((n >= 5) && is_atomic (p[4])) sz= as_int (p[4]);
   if ((n >= 6) && is_atomic (p[5])) dpi= as_int (p[5]);
   if ((n >= 7) && is_atomic (p[6])) dw= as_int (p[6]);
-  font fn= find_font (family, fn_class, series, shape, sz, dpi);
+  bool adjust= s == "<lefttimesthree>" || s == "<righttimesthree>";
+  font fn= smart_font (family, fn_class, series, shape, adjust ? 1.15 * sz : sz,
+                       dpi);
   box  b = text_box (decorate (), 0, s, fn, col);
   if (ink) b= resize_box (decorate (), b, b->x3, b->y3, b->x4, b->y4, true);
+  if (adjust) b= shift_box (decorate (), b, 0, -fn->wfn / 16);
   return box_widget (b, trans, dw);
 }
 
