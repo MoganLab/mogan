7e8cc3495c18d95cb3a049f08b5879e18b9325ab
Jim Zhou <jimzhouzzy@gmail.com>
2026-08-06 13:00:17 +0800

    rvt unessry

--- src/Texmacs/Window/tm_window.cpp
+++ src/Texmacs/Window/tm_window.cpp
@@ -16,7 +16,6 @@
 #include "message.hpp"
 #include "preferences.hpp"
 #include "tm_data.hpp"
-#include "tm_debug.hpp" // bench_start / bench_end
 #include "tm_url.hpp"
 
 #include <moebius/drd/drd_std.hpp>
@@ -407,9 +406,7 @@ tm_window_rep::get_menu_widget (int which, string menu, widget& w) {
     tm_view vw= concrete_view (window_to_view (id));
     if (vw != NULL) the_drd= vw->ed->drd;
   }
-  if (which == -1) bench_start ("menu-expand");
   object xmenu= call ("menu-expand", eval ("'" * menu));
-  if (which == -1) bench_end ("menu-expand");
   the_drd= old_drd;
   // tab 栏（which==4）：xmenu 含每次新建的 lambda，无法用 equal 比较，故用
   // 稳定签名判等。签名不变（如切 tab）=> 跳过重建，保持上次 widget。
@@ -428,7 +425,6 @@ tm_window_rep::get_menu_widget (int which, string menu, widget& w) {
   }
   menu_current (which)= xmenu;
   object umenu        = eval ("'" * menu);
-  if (which == -1) bench_start ("make_menu_widget");
   if (which == 10 || which == 11) {
     w= make_menu_widget (umenu, 400, 1000);
   }
