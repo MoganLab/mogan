c64d066b8fe1bde431437635007da015b298a7f6
Da Shen <da@liii.pro>
2026-08-14 00:57:09 +0800

    wip

--- TeXmacs/plugins/stem/progs/stem/stem-format.scm
+++ TeXmacs/plugins/stem/progs/stem/stem-format.scm
@@ -17,14 +17,9 @@
 ;; Scheme format for TeXmacs source files (no information loss)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define (stem-recognizes? s)
-  (and (string? s) (string-starts? s "(document (TeXmacs"))
-) ;define
-
 (define-format stem
   (:name "STEM")
   (:suffix "stem")
-  (:must-recognize stem-recognizes?)
 ) ;define-format
 
 (define (texmacs->stem t)
