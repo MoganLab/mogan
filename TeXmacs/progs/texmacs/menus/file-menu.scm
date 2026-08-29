edf80cf841cbf2a76394aa0e1940c43ae4d19469
sickwag <sickwag@outlook.com>
2026-08-29 19:36:04 +0800

    [2084] 按题目要求回退菜单入口接线
    菜单「打印为文件」保留原有 choose-file 系统保存框交互，仅快捷键
    C-F4 / M-S-F4 走 QML 对话框（PR 题目明确要求保留菜单入口原行为）。

--- TeXmacs/progs/texmacs/menus/file-menu.scm
+++ TeXmacs/progs/texmacs/menus/file-menu.scm
@@ -318,10 +318,10 @@
    ("Print page selection" (interactive print-pages))
   ) ;if
   ("Print buffer to file"
-    (open-print-to-file)
+    (choose-file print-to-file "Print all to file" "postscript")
   ) ;
   ("Print page selection to file"
-    (open-page-selection-to-file)
+    (interactive choose-file-and-print-page-selection)
   ) ;
 ) ;menu-bind
 
@@ -329,7 +329,7 @@
  ("Preview" (preview-buffer))
  (if (use-print-dialog?)
    (if (has-printing-cmd?) ("Print" (print-buffer)))
-   ("Print to file" (open-print-to-file))
+   ("Print to file" (choose-file print-to-file "Print all to file" "postscript"))
  ) ;if
  (if (not (use-print-dialog?)) (-> "Print" (link print-menu-sub)))
  (if (use-menus?) (-> "Page setup" (link page-setup-menu)))
@@ -340,7 +340,7 @@
  ("Preview" (preview-buffer))
  (if (use-print-dialog?)
    (if (has-printing-cmd?) ("Print" (print-buffer)))
-   ("Print to file" (open-print-to-file))
+   ("Print to file" (choose-file print-to-file "Print all to file" "postscript"))
  ) ;if
  (if (not (use-print-dialog?)) --- (link print-menu-sub) ---)
  (if (use-menus?) (-> "Page setup" (link page-setup-menu)))
