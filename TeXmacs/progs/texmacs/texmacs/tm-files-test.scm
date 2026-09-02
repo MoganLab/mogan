b10c68e3a831f61552fc95e6c4106d76c5875bfc
dwjgcyzj0830-crypto <dwjgcyzj0830@gmail.com>
2026-09-02 10:20:08 +0800

    [1257] 草稿文件名用 _ 分隔日期与时刻

--- TeXmacs/progs/texmacs/texmacs/tm-files-test.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-files-test.scm
@@ -17,6 +17,7 @@
 
 (import (liii check))
 (import (only (liii path) path-join path->string))
+(import (only (liii string) string-starts? string-ends? string-index))
 
 (check-set-mode! 'report-failed)
 
@@ -49,11 +50,51 @@
 ) ;define
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Tests for draft_YYYYMMDD_HHMMSS naming (1257)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (draft-test-url name) (system->url name))
+
+(define (draft-name-stamp-part name)
+  (let* ((body (substring name 6 (- (string-length name) 4)))
+         (cut (or (string-index body #\-) (string-length body)))
+        ) ;
+    (substring body 0 cut)
+  ) ;let*
+) ;define
+
+(define (test-scratch-buffer-name-has-date-time-underscore)
+  (let* ((path (scratch-buffer-name))
+         (name (url->string (url-tail (system->url path))))
+         (stamp (draft-name-stamp-part name))
+        ) ;
+    (check (string-starts? name "draft_") => #t)
+    (check (string-ends? name ".tmu") => #t)
+    ;; YYYYMMDD_HHMMSS → 长度 15,第 9 个字符是 _
+    (check (string-length stamp) => 15)
+    (check (substring stamp 8 9) => "_")
+  ) ;let*
+) ;define
+
+(define (test-scratch-buffer-title-old-and-new-stamp)
+  ;; 往年草稿不显示时刻,新旧文件名必须得到同一标题
+  (let ((old (scratch-buffer-title (draft-test-url "draft_20250802153000.tmu")))
+        (new (scratch-buffer-title (draft-test-url "draft_20250802_153000.tmu")))
+        (new-n (scratch-buffer-title (draft-test-url "draft_20250802_153000-1.tmu")))
+       ) ;
+    (check old => new)
+    (check new => new-n)
+  ) ;let
+) ;define
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Test entry point
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (tm-define (regtest-tm-files)
   (test-auto-backup-official-url)
   (test-auto-backup-texmacs-path-buffer?)
+  (test-scratch-buffer-name-has-date-time-underscore)
+  (test-scratch-buffer-title-old-and-new-stamp)
   (check-report)
 ) ;tm-define
