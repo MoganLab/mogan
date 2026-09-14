30761ef373323a08574ba0c0cded5e3e3603ea6c
MoonL79 <151734889+MoonL79@users.noreply.github.com>
2026-09-14 10:59:51 +0800

    [0526] OAuth 回调链路改造（三）：state 校验（CSRF 防护） (#4548)

--- src/Plugins/Qt/QTMOAuth.hpp
+++ src/Plugins/Qt/QTMOAuth.hpp
@@ -22,6 +22,7 @@
 #include <QtCore/qjsonobject.h>
 #include <QtCore/qpointer.h>
 #include <QtCore/qtimer.h>
+#include <QtCore/qvariant.h>
 
 QT_FORWARD_DECLARE_CLASS (QRestAccessManager)
 
@@ -45,12 +46,14 @@ private:
   void       loadExistingToken ();
   QString    generateCodeVerifier ();
   QString    generateCodeChallenge (const QString& verifier);
+  QString    generateRandomString (int length);
   QUrl       getAuthorizationUrl ();
   QUrl       getAccessTokenUrl ();
   QString    getRedirectUri ();
   QString    getGrowthUrl ();
   QByteArray getPreviewCookieHeader ();
   void       refreshCallbackHtml ();
+  void       handleCallback (const QVariantMap& values);
 
   bool m_isLoggedIn= false;
 
@@ -65,6 +68,8 @@ public:
   qint64                        m_tokenExpiryTime= 0;
   QString                       m_codeVerifier;
   QString                       m_codeChallenge;
+  QString                       m_instanceId;
+  QString                       m_state;
 };
 
 #endif
